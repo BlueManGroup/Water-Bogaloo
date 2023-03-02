99eb62c469aed7bf60fed53cf468d6564b092324
creich21 <creich21@student.aau.dk>
2023-03-02 15:15:18 +0100

    Partial update

--- DB/connection.js
+++ DB/connection.js
@@ -4,14 +4,18 @@ require('dotenv').config()
 //.env variables
 DBURI = process.env.DBURI
 
+const client = new MongoClient(DBURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 //MongoDB connection
-const client = new MongoClient(DBURI, { useNewUrlParser: true, useUnifiedTopology: true })
-client.connect(err => {
-    if (err) {
-      console.error(err);
-      process.exit(1);
+async function connectToDatabase() {
+    try {
+      await client.connect();
+      console.log("Connected to MongoDB!");
+    } catch (err) {
+      console.error("Failed to connect to MongoDB", err);
     }
-})
+  }
+  connectToDatabase();
 
 //Database connection
 const db = client.db("Slusen")
@@ -25,6 +29,25 @@ async function create(collection,data,) {
         throw new error("invalid collection")
     }
 }
+
+//Update operation **Needs input validation**
+async function update(collection,identifier,parameter,data) {
+    if(collection == "users") {
+      await db.collection("users").updateOne(
+        {_id: identifier},
+        {$set: {parameter: data}},
+        (err, result) => {
+          if (err) {
+            console.error(err);
+          } else {
+            console.log(result);
+          }  
+        }
+      );
+    }
+
+}
+
 
 module.exports = {
     create,
