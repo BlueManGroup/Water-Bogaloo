249b531decf6facc3dd636df412d837e2eddf300
CactusLlama <58257787+CactusLlama@users.noreply.github.com>
2023-03-24 13:41:36 +0100

    whyyyy it no work!!!

--- DB/connection.js
+++ DB/connection.js
@@ -91,7 +91,7 @@ async function read(collection, identifier, fields) {
     switch(Object.keys(identifier)[0]) {
 
         case "username":
-            console.log(identifier.username);
+            console.log(identifier);
             iobject = {username: identifier.username};
             break;
 
