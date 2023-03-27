0d9886a49e0ae4611a5aeffde2cc311d4c1cbdee
creich21 <creich21@student.aau.dk>
2023-03-27 18:03:10 +0200

    error handling in JWT functions

--- Tokens/JWT.js
+++ Tokens/JWT.js
@@ -4,26 +4,41 @@ const secret = process.env.TOKENSECRET
 
 const createToken = (user) => {
     // create token with following parameters and return
-    const token = sign(
-        {userId: user._id, username:user.username},
-        secret,
-        {expiresIn:"1h"}
-    )
-    return token
+    try {
+        const token = sign(
+            {userId: user._id, username:user.username},
+            secret,
+            {expiresIn:"1h"}
+            
+        )
+        return token
+    } catch (e) {
+        console.error(e)
+        return
+    }
+    
 }
 
 const verifyToken = (token) => {
     // verify token and assign to variable
-    let verification = verify(token, secret);
-
+    try {
+        let verification = verify(token, secret);
+    } catch (e) {
+        console.error(e)
+        return false
+    }
     // return true or false based on whether or not the token was validated
     return verification ? true : false;
 }
 
 const decodeToken = (token) => {
-    let decodedToken = decode(token);
-    
-    return decodedToken
+    try {
+        let decodedToken = decode(token);
+        return decodedToken
+    } catch (e) {
+        console.error(e)
+        return
+    }
 } 
 
 module.exports = { 
