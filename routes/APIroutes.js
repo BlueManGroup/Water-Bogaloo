f970d63507d8d979d8b2a59b27c03f77f6c80fe5
Johannes Hansen <92086260+Joha00@users.noreply.github.com>
2023-05-02 10:54:41 +0200

    test again

--- routes/APIroutes.js
+++ routes/APIroutes.js
@@ -53,7 +53,7 @@ router.post('/login', async (req, res) =>{
             response: "invalid input"
         });
     }
-    console.log(data)
+    console.log(data.password)
     data.password = hashPassword(data.password);
     const fields = {username:1,password:1,role:1}
     let user;
